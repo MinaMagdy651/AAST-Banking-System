f7b903deebb1f8e4af5e0bacfe9f463119bd8fe7
MOSTAFA abdELSAMAD <M.A.abdELSAMAD@student.aast.edu>
2021-12-18 19:08:37 +0200

    update 3

--- Project/lgnfrm.cs
+++ Project/lgnfrm.cs
@@ -31,7 +31,7 @@ namespace Project
 
         private void btn_lgin_Click(object sender, EventArgs e)
         {
-            //update 2
+            //update 3
             Person p = new Person(Convert.ToUInt32(usernametxt.Text), passwordtxt.Text);
 
 
