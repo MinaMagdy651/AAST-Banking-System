f2e614e2ba839a1d42ba1963796f1a97d82e6b69
Magdy Kamel <Magdy Kamel@DESKTOP-C2QAJ0D>
2021-12-18 15:38:14 +0200

    New form

---
+++ Project/lgnfrm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Project
+{
+    partial class lgnfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.usernametxt = new System.Windows.Forms.TextBox();
+            this.passwordtxt = new System.Windows.Forms.TextBox();
+            this.btn_lgin = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Location = new System.Drawing.Point(81, 25);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(60, 15);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Username";
+            this.lbl_username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(81, 78);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(57, 15);
+            this.lbl_password.TabIndex = 1;
+            this.lbl_password.Text = "Password";
+            // 
+            // usernametxt
+            // 
+            this.usernametxt.Location = new System.Drawing.Point(155, 22);
+            this.usernametxt.Name = "usernametxt";
+            this.usernametxt.Size = new System.Drawing.Size(100, 23);
+            this.usernametxt.TabIndex = 2;
+            // 
+            // passwordtxt
+            // 
+            this.passwordtxt.Location = new System.Drawing.Point(155, 70);
+            this.passwordtxt.Name = "passwordtxt";
+            this.passwordtxt.Size = new System.Drawing.Size(100, 23);
+            this.passwordtxt.TabIndex = 3;
+            // 
+            // btn_lgin
+            // 
+            this.btn_lgin.Location = new System.Drawing.Point(86, 125);
+            this.btn_lgin.Name = "btn_lgin";
+            this.btn_lgin.Size = new System.Drawing.Size(75, 23);
+            this.btn_lgin.TabIndex = 4;
+            this.btn_lgin.Text = "Login";
+            this.btn_lgin.UseVisualStyleBackColor = true;
+            this.btn_lgin.Click += new System.EventHandler(this.btn_lgin_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(180, 125);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lgnfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(365, 174);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_lgin);
+            this.Controls.Add(this.passwordtxt);
+            this.Controls.Add(this.usernametxt);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_username);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "lgnfrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Page";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_username;
+        private Label lbl_password;
+        private TextBox usernametxt;
+        private TextBox passwordtxt;
+        private Button btn_lgin;
+        private Button btn_exit;
+    }
+}
---
+++ Project/lgnfrm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class lgnfrm : Form
+    {
+        public lgnfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_lgin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Testing btn");
+        }
+    }
+}
