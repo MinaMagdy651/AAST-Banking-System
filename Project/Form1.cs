2d459183c2c080c7afb6f16921e87fa53f624ed5
Mina Magdy <minamagdy651@gmail.com>
2022-01-14 04:27:32 +0200

    exception handling

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -65,27 +65,19 @@ namespace Project
             try
             {
 
-                if (!string.IsNullOrEmpty(textDeposit.Text) && textDeposit.Text.All(char.IsNumber))
-                {
-                    double val = Double.Parse(textDeposit.Text);
+                double val = Double.Parse(textDeposit.Text);
 
-                    if (user1.Deposit(val))
-                    {
-                        dep_with_stat.Text = "Succesful transaction";
-                        UserBalanceLab.Text = Convert.ToString(user1.Balance);
-                        textDeposit.Clear();
-                    }
-                    else
-                    {
-                        textDeposit.Clear();
-                        dep_with_stat.Text = "Please enter a valid amount";
-                    }
+                if (user1.Deposit(val))
+                {
+                    dep_with_stat.Text = "Succesful transaction";
+                    UserBalanceLab.Text = Convert.ToString(user1.Balance);
                 }
+
                 else
                 {
-                    textDeposit.Clear();
                     dep_with_stat.Text = "Please enter a valid amount";
                 }
+                textDeposit.Clear();
                 lbl_expBal.Text = Convert.ToString(user1.GetBalance());
             }
             catch (Exception ex)
@@ -100,19 +92,14 @@ namespace Project
         {
             try
             {
-                if (!string.IsNullOrEmpty(textWithdraw.Text) && textWithdraw.Text.All(char.IsNumber))
+
+                double val = Double.Parse(textWithdraw.Text);
+                if (user1.Withdraw(val))
                 {
-                    double val = Double.Parse(textWithdraw.Text);
-                    if (user1.Withdraw(val))
-                    {
-                        dep_with_stat.Text = "Succesful transaction";
-                        UserBalanceLab.Text = Convert.ToString(user1.Balance);
-
-                    }
-                    else
-                        dep_with_stat.Text = "Please enter a valid amount";
-
+                    dep_with_stat.Text = "Succesful transaction";
+                    UserBalanceLab.Text = Convert.ToString(user1.Balance);
                 }
+
                 else
                 {
                     dep_with_stat.Text = "Please enter a valid amount";
@@ -131,35 +118,26 @@ namespace Project
         {
             try
             {
-                if (!string.IsNullOrEmpty(TxtTranAmount.Text) && !string.IsNullOrEmpty(TxtTranUser.Text)
-                    && TxtTranAmount.Text.All(char.IsNumber) && TxtTranUser.Text.All(char.IsNumber))
+
+                double val = Double.Parse(TxtTranAmount.Text);
+                UInt32 user2_acc_num = UInt32.Parse(TxtTranUser.Text);
+                User user2 = new User(user2_acc_num);
+                if (user1.Account_Number == user2.Account_Number)
                 {
-                    double val = Double.Parse(TxtTranAmount.Text);
-                    UInt32 user2_acc_num = UInt32.Parse(TxtTranUser.Text);
-                    User user2 = new User(user2_acc_num);
-                    if (user1.Account_Number == user2.Account_Number)
-                    {
-                        MessageBox.Show("Can not transfer money to your own account");
-                        TxtTranUser.Clear();
-                    }
-                    else if (user1.Transfer(user2, val))
-                    {
-                        transferstat.Text = string.Format("Transfered ${0} to: {1}", val, user2.Name);
-                        UserBalanceLab.Text = Convert.ToString(user1.Balance);
-                    }
-                    else
-                    {
-                        transferstat.Text = "Insufficient or account doesn't exist";
-                        TxtTranUser.Clear();
-                        TxtTranAmount.Clear();
-                        lbl_expBal.Text = Convert.ToString(user1.GetBalance());
-                    }
+                    MessageBox.Show("Can not transfer money to your own account");
+                    TxtTranUser.Clear();
+                }
+                else if (user1.Transfer(user2, val))
+                {
+                    transferstat.Text = string.Format("Transfered ${0} to: {1}", val, user2.Name);
+                    UserBalanceLab.Text = Convert.ToString(user1.Balance);
                 }
                 else
                 {
+                    transferstat.Text = "Insufficient or account doesn't exist";
                     TxtTranUser.Clear();
                     TxtTranAmount.Clear();
-                    MessageBox.Show("Pleanser Enter valid data");
+                    lbl_expBal.Text = Convert.ToString(user1.GetBalance());
                 }
             }
             catch (Exception ex)
@@ -175,27 +153,21 @@ namespace Project
         {
             try 
             {
-                if (!string.IsNullOrEmpty(txtbox_askLoan.Text) && txtbox_askLoan.Text.All(char.IsNumber))
+ 
+                double val = Double.Parse(txtbox_askLoan.Text);
+                if (user1.AskLoan(val))
                 {
-                    double val = Double.Parse(txtbox_askLoan.Text);
-                    if (user1.AskLoan(val))
-                    {
-                        UserBalanceLab.Text = Convert.ToString(user1.Balance);
-                        lbl_debt.Text = Convert.ToString(user1.Debt);
-                        askLoan_stat.Text = string.Format("Succesfully added ${0} to your account", val);
-                    }
-                    else
-                    {
-                        askLoan_stat.Text = "You are not eligble for this loan";
-                        txtbox_askLoan.Clear();
-                        lbl_expBal.Text = Convert.ToString(user1.GetBalance());
-                    }
+                    UserBalanceLab.Text = Convert.ToString(user1.Balance);
+                    lbl_debt.Text = Convert.ToString(user1.Debt);
+                    askLoan_stat.Text = string.Format("Succesfully added ${0} to your account", val);
                 }
                 else
                 {
-                    MessageBox.Show("Pleas enter a valid amount");
+                    askLoan_stat.Text = "You are not eligble for this loan";
                     txtbox_askLoan.Clear();
+                    lbl_expBal.Text = Convert.ToString(user1.GetBalance());
                 }
+                
             }
             catch (Exception)
             {
@@ -209,27 +181,18 @@ namespace Project
         {
             try
             {
-                if (!string.IsNullOrEmpty(payDebt.Text) && payDebt.Text.All(char.IsNumber))
+                double val = Double.Parse(payDebt.Text);
+                if (user1.Debt == 0)
                 {
-                    double val = Double.Parse(payDebt.Text);
-                    if (user1.Debt == 0)
-                    {
-                        label4.Text = "Your debt amout is 0$";
-                        payDebt.Clear();
-                    }
+                    label4.Text = "Your debt amout is 0$";
+                    payDebt.Clear();
+                }
 
-                    else if (user1.paydebt(val))
-                    {
-                        UserBalanceLab.Text = Convert.ToString(user1.Balance);
-                        lbl_debt.Text = Convert.ToString(user1.Debt);
-                        label4.Text = "Completed Successfully";
-                    }
-                    else
-                    {
-                        label4.Text = "Please Enter valid amout";
-                        payDebt.Clear();
-                    }
-
+                else if (user1.paydebt(val))
+                {
+                    UserBalanceLab.Text = Convert.ToString(user1.Balance);
+                    lbl_debt.Text = Convert.ToString(user1.Debt);
+                    label4.Text = "Completed Successfully";
                 }
                 else
                 {
