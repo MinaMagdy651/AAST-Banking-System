34b953f89028857a3b30bddee5e29bdb11273fd8
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2022-03-09 14:45:27 +0200

    Changed the name

--- Project/Properties/Resources.Designer.cs
+++ Project/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project.Properties {
+namespace AAST_Banking_System.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Project.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AAST_Banking_System.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
